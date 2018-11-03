d8a5b02d3b0b6be18fa5f2384d65b7499fb0a83b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-03 03:32:36 +0000

    automatically backed up by learn

---
+++ src/components/Band.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Band extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id,
+    });
+  }
+
+  render() {
+    return (
+      <li>
+        {this.props.band.text}
+        <button onClick={this.handleOnClick} />
+      </li>
+    );
+  }
+};
+
+export default Band;
